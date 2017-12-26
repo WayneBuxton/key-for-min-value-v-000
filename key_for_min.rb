a41f2ab0a9d9ce778986862bc571d15bfd63071c
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-12-26 18:03:17 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ require "pry"
 def key_for_min_value(name_hash)
   lowest_value = nil
   name_hash.collect do |key,value|
-    binding.pry
+  #  binding.pry
   if lowest_value == nil
     lowest_value = value
   end
